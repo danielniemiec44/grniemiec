d519c76dccddb494616f3336926b48ea540f21a8
danielniemiec44@wp.pl <danielniemiec44@gmail.com>
2020-01-24 18:58:32 +0100

    No Frontend

--- panel/index.php
+++ panel/index.php
@@ -25,6 +25,12 @@ if (!$link) {
   width: 100vw;
   height: 100vh
 }
+
+.navbar {
+  background: #8a9d23
+}
+
+
 </style>
 </head>
 <body>
@@ -35,7 +41,7 @@ if (!$link) {
 
 
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark">
   <a class="navbar-brand" href="#">GRNIEMIEC Panel</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -44,12 +50,9 @@ if (!$link) {
   <div class="collapse navbar-collapse" id="navbarColor02">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Strona Główna <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Strona Główna<span class="sr-only">(current)</span></a>
       </li>
     </ul>
-    
-      
-      <button class="btn btn-secondary my-2 my-sm-0" id="logInWindowButton">Zaloguj się</button>
     
   </div>
 </nav>
@@ -57,64 +60,7 @@ if (!$link) {
 
 
 
-<div class="modal fade" id="logInWindow">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Zaloguj się</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        
 
-
-
-      <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-
-
-
-
-      </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-primary">Zaloguj się</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-
-
-<div class="page">
-a
-</div>
-
-<div class="page">
-b
-</div>
-
-<div class="page">
-c
-</div>
-
-
-<script>
-$("#logInWindowButton").click(showLogInWindow);
-
-function showLogInWindow(){
-$("#logInWindow").modal();
-}
-</script>
 
 
 
